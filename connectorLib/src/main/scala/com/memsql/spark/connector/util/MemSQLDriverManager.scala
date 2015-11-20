2071557d9b7763f26d1e90fe63e3308a7c3b6dac
Hurshal Patel <hurshal@memsql.com>
2015-11-20 10:41:19 -0800

    remove erroneous comment in MemSQLDriverManager

--- connectorLib/src/main/scala/com/memsql/spark/connector/util/MemSQLDriverManager.scala
+++ connectorLib/src/main/scala/com/memsql/spark/connector/util/MemSQLDriverManager.scala
@@ -5,7 +5,6 @@ import java.sql.{Connection, DriverManager}
 object MemSQLDriverManager {
   val DEFAULT_JDBC_LOGIN_TIMEOUT = 10 //seconds
 
-  // set a timeout and try to connect 3 times before throwing an exception
   def getConnection(dbAddress: String, user: String, password: String): Connection = {
     DriverManager.setLoginTimeout(DEFAULT_JDBC_LOGIN_TIMEOUT)
     DriverManager.getConnection(dbAddress, user, password)
